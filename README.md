396e0cf197df6430455fd78a032bb333828cdc6f
Maxwell Turner <maxt992@gmail.com>
2022-06-09 10:34:33 -0700

    working on variable functions, credits added to readme

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 ## CREDITS
 
-- Foolish Developer @ https://dev.to/code_mystery/random-password-generator-using-javascript-6a
 - UW Coding Bootcamp Team
 - Anthony Brown
+- Foolish Developer @ https://dev.to/code_mystery/random-password-generator-using-javascript-6a
+- Pointy @ https://stackoverflow.com/questions/6454198/check-if-a-value-is-within-a-range-of-numbers
--- script.js
+++ script.js
@@ -30,7 +30,13 @@ function generatePassword() {
   // Prompt the user for their password length and store it in a variable
   var numLength = prompt("How many characters do you want your password to contain? Please choose a number between 8 and 128.");
   // Validate the password length and make sure it's a number between or equeal to 8 and 128
-
+  function between(numLength, min, max) {
+    return numLength >= min && x <= max;
+  }
+  // TODO Prompt confirmation? Here?
+  if (between(numLength, 8, 128)) {
+    // something
+  }
   // Prompt the user for if they want to use special characters
 }
 
