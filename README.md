82bb5a0d5603a2d4ab36b43de0dc3a361ee39df7
Maxwell Turner <maxt992@gmail.com>
2022-06-09 10:18:28 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+## CREDITS
+
+- Foolish Developer @ https://dev.to/code_mystery/random-password-generator-using-javascript-6a
+- UW Coding Bootcamp Team
+- Anthony Brown
---
+++ script.js
@@ -0,0 +1,56 @@
+
+// Defined charset for password generation.
+var undercase = ["abcdefghijklmnopqrstuvwxyz"];
+var uppercase = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
+var characters = ["!@#$%^&*()"];
+// var numbers = [0123456789].split("");
+var numbers = ["0123456789"];
+// Defined password min/max length.
+var passwordLength = {
+  from: 8,
+  to: 128
+};
+
+
+// console.log(undercase);
+// console.log(uppercase);
+// console.log(characters);
+// console.log(numbers);
+// console.log(passwordLength);
+// console.log(password);
+
+
+
+
+// What sorts of actions/tasks do my code need to complete?
+
+// Generate Password
+function generatePassword() {
+  
+  // Prompt the user for their password length and store it in a variable
+  var numLength = prompt("How many characters do you want your password to contain? Please choose a number between 8 and 128.");
+  // Validate the password length and make sure it's a number between or equeal to 8 and 128
+
+  // Prompt the user for if they want to use special characters
+}
+
+
+
+
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/concat
